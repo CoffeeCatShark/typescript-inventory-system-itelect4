4b1d10f168066956d03b7246d125c08741af5967
CoffeeStrike <omniexpansiontheory@gmail.com>
2026-07-22 20:42:51 +0800

    league of legends

---
+++ src/viewmodels/AddManagerPage.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import type { Manager } from "../types/types"
+import { add } from "../data/helpers"
+import { globalID, incrementID } from "../data/database"
+
+interface AddManagerPageProps {
+    managerList: Manager[];
+    setManagerList: React.Dispatch<React.SetStateAction<Manager[]>>;
+}
+
+function AddManagerPage({
+    managerList,
+    setManagerList,
+}: AddManagerPageProps) {
+    return (<>
+    
+    
+    
+    </>)
+}
